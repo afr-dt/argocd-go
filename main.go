852ca93cfc6a39aaad94cb2d749f42355401938c
Alejandro Flores <aleksgnu@gmail.com>
2024-02-27 14:35:06 -0600

    feat: added healthz & readyz

--- main.go
+++ main.go
@@ -6,12 +6,27 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "ArgoCD Golang!")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "K8s ArgoCD Golang!")
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "ok")
+}
+
+func readyzHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "ok")
 }
 
 func main() {
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/healthz", healthzHandler)
+	http.HandleFunc("/readyz", readyzHandler)
+
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		fmt.Printf("Sever error: %v\n", err)
 	}
+
 }
